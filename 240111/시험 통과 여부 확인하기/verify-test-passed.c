89e8ce06634521e823c0b064dd79ecca0fcf56b5
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-11 05:02:20 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/시험 통과 여부 확인하기/verify-test-passed.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    if(n >= 80){
+        printf("pass\n");
+    }else{
+        printf("%d more score", 80-n);
+    }
+    return 0;
+}
