8ba48159bb391b9fa21147b69eda7bb4b0a000d4
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-11 05:03:47 +0900

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/숫자의 조건 여부/number's-condition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d", &a);
+
+    if(a >= 113){
+        printf("1", a);
+    }else{
+        printf("0", a);
+    }
+    return 0;
+}
