94220631e7880859837d202252546c401f75c438
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-11 04:50:21 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240111/음수 구별하기/separate-negative-number.c
+++ 240111/음수 구별하기/separate-negative-number.c
@@ -3,8 +3,8 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
-    scanf("%d", n);
-    printf("%d\n", &n);
+    scanf("%d", &n);
+    printf("%d\n", n);
 
     if(n < 0){
         printf("minus");
