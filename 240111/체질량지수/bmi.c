9507744af5051e96efeff0951095827a1b61d666
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-11 04:57:08 +0900

    Title: 체질량지수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240111/체질량지수/bmi.c
+++ 240111/체질량지수/bmi.c
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int 키, 몸무게;
     scanf("%d %d", &키, &몸무게);
-    BMI = 몸무게/키*키
+    int BMI = 몸무게*100*100/(키*키);
 
     printf("%d", BMI);
     if(BMI >= 25){
