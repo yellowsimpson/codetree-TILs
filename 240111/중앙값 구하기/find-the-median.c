e3b57d1ad25793263624759efc0eb774e98ed33a
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-11 17:28:33 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240111/중앙값 구하기/find-the-median.c
+++ 240111/중앙값 구하기/find-the-median.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
 
-    if(a > b){
-        if(b > c){
-            printf("%d", &b);
-        }else
-            printf("%d", &c);
-    }else(a < b){
-        if(a > c){
-            printf("%d", &a);
-        }else
-            printf("%d", &c);
+    // 중앙값 찾기
+    int middle;
+
+    if ((a >= b && a <= c) || (a <= b && a >= c)) {
+        middle = a;
+    } else if ((b >= a && b <= c) || (b <= a && b >= c)) {
+        middle = b;
+    } else {
+        middle = c;
     }
+
+    // 중앙값 출력
+    printf("%d", middle);
+
     return 0;
 }
