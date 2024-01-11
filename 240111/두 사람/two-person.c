9dc4743bb5be7fcf9aa36350cf20c80f6f098d95
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-11 16:36:48 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240111/두 사람/two-person.c
+++ 240111/두 사람/two-person.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int a, b;
     char a1, b1;
-    scanf("%d %c\n %d %c", &a, &a1, &b, &b1);
+    scanf("%d %c\n", &a, &a1);
+    scnaf(" %d %c", &b, &b1);
 
-    if((a >= 19 || b >= 19) && (a1 == 'm' || b1 == 'm')){
+    if((a >= 19 && b >= 19) || (a1 == 'm' && b1 == 'm')){
         printf("1");
     }else
         printf("0");
