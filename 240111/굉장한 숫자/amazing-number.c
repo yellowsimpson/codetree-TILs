b7565677daccdd9df706884e2a6acff25cffdef9
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-11 10:02:58 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240111/굉장한 숫자/amazing-number.c
+++ 240111/굉장한 숫자/amazing-number.c
@@ -5,7 +5,7 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    if((n % 2 == 1 || n % 3 == 0) && (n % 2 == 0 || n % 5 == 0)){
+    if((n % 2 == 1 && n % 3 == 0) || (n % 2 == 0 && n % 5 == 0)){
         printf("true");
     }else{
         printf("flase");
