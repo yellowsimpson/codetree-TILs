f2f4989d4955f815ec9c8684f1e61494a69f7748
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-11 17:04:50 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240111/두 사람/two-person.c
+++ 240111/두 사람/two-person.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int age1, age2;
-    char gender1, gender2;
+    int a, b;
+    char a1, b1;
+    scanf("%d %c", &a, &a1);
+    scanf("%d %c", &b, &b1);
 
-    // 첫 번째 사람 정보 입력
-    scanf("%d %c", &age1, &gender1);
-
-    // 두 번째 사람 정보 입력
-    scanf("%d %c", &age2, &gender2);
-
-    // 한 사람이라도 19세 이상이면서 남자인 경우 1 출력, 그 외에는 0 출력
-    if ((age1 >= 19 && gender1 == 'M') || (age2 >= 19 && gender2 == 'M')) {
+    if((a >= 19 && b >= 19) || (a1 == 'm' && b1 == 'm')){
         printf("1\n");
-    } else {
+    }else
         printf("0\n");
-    }
-
     return 0;
 }
