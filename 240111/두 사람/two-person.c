d2acb4533915ade2a82277b09d3b820b09135244
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-11 16:00:57 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240111/두 사람/two-person.c
+++ 240111/두 사람/two-person.c
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
     char a1, b1;
-    scanf("%d %c\n %d %c", &a, &a1, %b, %b1);
+    scanf("%d %c\n %d %c", &a, &a1, &b, &b1);
 
     if((a >= 19 && b >= 19) || (a1 == 'm' && b1 == 'm')){
         printf("1");
