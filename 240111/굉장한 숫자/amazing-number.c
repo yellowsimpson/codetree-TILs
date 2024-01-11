774be9a42e391d4dbebf86ecbd832a66785caafe
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-11 10:03:44 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240111/굉장한 숫자/amazing-number.c
+++ 240111/굉장한 숫자/amazing-number.c
@@ -7,8 +7,8 @@ int main() {
 
     if((n % 2 == 1 && n % 3 == 0) || (n % 2 == 0 && n % 5 == 0)){
         printf("true");
-    }else{
+    }else
         printf("flase");
-    }
+    
     return 0;
 }
