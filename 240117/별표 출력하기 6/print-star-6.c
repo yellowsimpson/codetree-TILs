ab7aeecff4098895941bc3a5232bf7f645977fa8
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-17 17:07:56 +0900

    Title: 별표 출력하기 6, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240117/별표 출력하기 6/print-star-6.c
+++ 240117/별표 출력하기 6/print-star-6.c
@@ -6,9 +6,19 @@ int main() {
     scanf("%d", &n);
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            printf("*");
+        for(int j = 0; j < i; j++){
+            printf("  ");
+        for(int j = 0; j < (2 * n) - (2 * i) - 1; j++)
+                printf("* ");
+            printf("\n");
         }
+    }
+
+    for(int i = n-2; i >= 0; i--){
+        for(int j = 0; j < i; j++)
+            printf("  ");
+        for(int j = 0; j < (2 * n) - (2 * i) - 1; j++)
+            printf("* ");
         printf("\n");
     }
     return 0;
