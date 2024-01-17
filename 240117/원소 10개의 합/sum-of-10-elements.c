10e017901bd5cc42c20d8c2cb20c5fa1203795c3
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-17 17:55:09 +0900

    Title: 원소 10개의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240117/원소 10개의 합/sum-of-10-elements.c
+++ 240117/원소 10개의 합/sum-of-10-elements.c
@@ -4,13 +4,15 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int arr[10];
     int sum = 0;
-    
-    for(int i = 0; i < 10; i++){
+
+    for(int i = 0; i < 9; i++){
         scanf("%d", &arr[i]);
     }
-    for(int i = 0; i < 10; i++){
+
+    for(int i = 0; i < 9; i++){
         sum += arr[i];
     }
-    printf("%d", sum);
+
+    printf("%d", &)
     return 0;
 }
