53b32e9c9c25a4ab4fa62df29b23590ce15c6051
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-17 17:11:26 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240117/원소 10개의 합/sum-of-10-elements.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int sum = 0;
+    
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &arr[i]);
+    }
+    for(int i = 0; i < 10; i++){
+        sum += arr[i];
+    }
+    printf("%d", sum);
+    return 0;
+}
