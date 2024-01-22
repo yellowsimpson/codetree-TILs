e31971f9a54482eca784e65ab805b54f58f4260f
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-22 12:45:26 +0900

    Title: 배열의 합, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240122/배열의 합/sum-of-array.py
+++ 240122/배열의 합/sum-of-array.py
@@ -1,7 +1,25 @@
-arr_2d = [[1, 2, 3, 4], 
-    [7, 8, 9, 10],
-    [11, 12, 13, 14], 
-    [15, 16, 17, 18]]
+# arr_2d = [
+#     [1, 2, 3, 4], 
+#     [7, 8, 9, 10], 
+#     [11, 12, 13, 14], 
+#     [15, 16, 17, 18]
+# ]
 
- print(arr_2d[0][0])
- print(arr_2d[1][2])
+# print(arr_2d[0][0]) # 1행 1열 : 1
+# print(arr_2d[1][2]) # 2행 3열 : 9
+
+# arr_2d = []
+# for i in range(4):
+#     arr = list(map(int, input().split()))
+#     arr_2d.append(arr)
+
+# arr_2d = []
+# for _ in range(4):
+#     arr_2d.append(list(map(int, input().split())))
+
+arr_2d = [
+    list(map(int, input().split()))
+    for _ in range(4)
+]
+
+print(arr_2d) # [[1, 2, 3, 4], [7, 8, 9, 10], [11, 12, 13, 14], [15, 16, 17, 18]]
