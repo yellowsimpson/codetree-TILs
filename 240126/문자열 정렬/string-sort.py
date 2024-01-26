b72b776f7722d275cccc36f7aa46b7640dea8bb4
yellowsimpson <137969623+yellowsimpson@users.noreply.github.com>
2024-01-26 22:52:49 +0900

    Title: 문자열 정렬, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240126/문자열 정렬/string-sort.py
+++ 240126/문자열 정렬/string-sort.py
@@ -1,3 +1,4 @@
+'''
 n = int(input())
 words = [input() for _ in range(n)]
 
@@ -5,3 +6,19 @@ words.sort()
 
 for word in words:
     print(word)
+'''
+
+# 변수 선언 및 입력
+# 예 : banana
+s = input()
+
+# 각 문자를 원소로 담는 리스트 만들기
+# 예 : ['b', 'a', 'n', 'a', 'n', 'a']
+sorted_list = sorted(s)
+
+# 각 문자를 원소로 갖는 리스트를 하나의 문자열로 합치기
+# 예 : 'banana'
+sorted_str = ''.join(sorted_list)
+
+# 출력
+print(sorted_str)
