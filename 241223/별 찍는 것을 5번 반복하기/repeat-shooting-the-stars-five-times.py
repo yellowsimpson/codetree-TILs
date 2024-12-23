d3fe12c19c1f699827fdb3272907dd6ee4499ab5
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2024-12-23 11:25:32 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241223/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.py
@@ -0,0 +1,10 @@
+# print("**********")
+# print("**********") 
+# print("**********") 
+# print("**********") 
+# print("**********") 
+
+def print_10_stars():
+    print("*" * 10)
+for _ in range(5):
+    print_10_stars()
