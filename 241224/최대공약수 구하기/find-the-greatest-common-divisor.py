2aa8b91215a19317652e26c22fef8594e0784782
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2024-12-24 11:01:20 +0900

    Title: 최대공약수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241224/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,13 @@
+n , m = tuple(map(int, input().split()))
+
+def find_gcd(n, m):
+    gcd = 0
+    for i in range(1, min(n, m) + 1):
+        if n % i == 0 and m % i == 0:
+            gcd = i
+    
+    print(gcd)
+
+find_gcd(n, m)
+
+
