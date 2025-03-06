631d7ca416f85cd680bb8f2e575e8079df57709d
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-03-06 12:00:45 +0900

    Title: 세 정수형 변수 선언, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250306/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 7;
+    int b = 23;
+    int c = a + b;
+    cout << a << " + " << b << " = " << c << endl;
+
+    
+    return 0;
+}
