b9f51f8582ab0d0f7295ec29cb7346737cebdb18
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-03-06 12:04:22 +0900

    Title: 변수 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250306/변수 출력하기 2/outputing-variables-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    string b = "C";
+
+    cout << b << "!.....!" << a << endl;
+    return 0;
+}
