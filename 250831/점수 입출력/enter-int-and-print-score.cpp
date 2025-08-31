43969d42fc1f608133dee6871d76c616197aa8c6
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-08-31 11:10:42 +0900

    Title: 점수 입출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250831/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+
+    cout << "Your score is " << N << " point." << endl;
+    return 0;
+}
