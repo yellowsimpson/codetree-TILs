156e3ff83fe8e80376265fd3261b402e6c8bb876
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-08-31 19:50:58 +0900

    Title: 합과 평균의 차, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250831/합과 평균의 차/sub-of-average-and-sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("%d\n%d\n%d", a+b+c, (a+b+c)/3, (a+b+c) - (a+b+c)/3);
+    return 0;
+}
