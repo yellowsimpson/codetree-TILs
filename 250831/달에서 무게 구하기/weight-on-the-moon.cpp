2a41726f8591e7ed0f90038842d18860697fc698
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-08-31 10:53:51 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250831/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int chu = 13;
+    float gravity = 0.165;
+
+    cout << fixed;
+    cout.precision(6);
+    cout << chu << " * " << gravity << " = " << chu*gravity << endl;
+    return 0;
+}
