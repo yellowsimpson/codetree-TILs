c67ec65b9006022775ba1c694b80c3ee90d9ad50
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-08-31 10:51:22 +0900

    Title: 달에서 무게 구하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250831/달에서 무게 구하기/weight-on-the-moon.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int chu = 13;
+    float gravity = 0.165;
+
+    printf("%d * %.6lf = %.6lf", chu, gravity, chu * gravity);
+
+    return 0;
+}
