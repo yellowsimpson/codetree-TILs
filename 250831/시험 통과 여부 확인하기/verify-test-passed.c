43d828311da8d61d310b686345a6e9dbbe17806e
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-08-31 23:48:17 +0900

    Title: 시험 통과 여부 확인하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250831/시험 통과 여부 확인하기/verify-test-passed.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d", &n);
+
+    if(n >= 80){
+        printf("pass");
+    }else
+        printf("%d more score", 100 - n);
+    
+    return 0;
+}
