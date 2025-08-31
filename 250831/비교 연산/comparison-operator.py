5d4d5e420867bef7a63109e5176e6ffc712b2d5f
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-08-31 23:41:55 +0900

    Title: 비교 연산, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250831/비교 연산/comparison-operator.py
@@ -0,0 +1,29 @@
+a, b = map(int, input().split())
+
+
+if (a >= b):
+    print(1)
+else:
+    print(0)
+if(a > b):
+    print(1)
+else:
+    print(0)
+if(a <= b):
+    print(1)
+else:
+    print(0)
+if(a < b):
+    print(1)
+else:
+    print(0)
+if(a == b):
+    print(1)
+else:
+    print(0)
+if(a != b):
+    print(1)
+else:
+    print(0)
+
+    
