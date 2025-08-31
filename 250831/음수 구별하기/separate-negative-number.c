d4b24eb83c67bf85871f4643a2fbabd626811957
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-08-31 20:17:01 +0900

    Title: 음수 구별하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250831/음수 구별하기/separate-negative-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d", &a);
+
+    if(a > 0){
+        printf("even");
+    }
+    else
+        printf("%d\nminus", a);
+    return 0;
+}
