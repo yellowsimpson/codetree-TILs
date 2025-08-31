9b2afcd13ed8ce181f3a5154291722725a9a2406
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-08-31 23:46:23 +0900

    Title: 비교 연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250831/비교 연산/comparison-operator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if(a >= b){
+        printf("1\n");
+    }else
+        printf("0\n");
+    if(a > b){
+            printf("1\n");
+        }else
+            printf("0\n");
+    if(a <= b){
+            printf("1\n");
+        }else
+            printf("0\n");
+    if(a < b){
+            printf("1\n");
+        }else
+            printf("0\n");
+    if(a == b){
+            printf("1\n");
+        }else
+            printf("0\n");
+    if(a != b){
+            printf("1\n");
+        }else
+            printf("0\n");
+
+  
+    return 0;
+}
