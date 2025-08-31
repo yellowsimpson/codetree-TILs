db7471e56a8b750e1e72ad637b86e46f0f6dad5d
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-08-31 13:06:25 +0900

    Title: 입력받은 값 교체하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250831/입력받은 값 교체하기/changing-inputs.cpp
+++ 250831/입력받은 값 교체하기/changing-inputs.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     // Please write your code here.
     int a, b;
-    cin >> a >> " ">> b;
+    cin >> a >> b;
 
-    cout << b << a << endl;
+    cout << b << " " << a << endl;
     return 0;
 }
