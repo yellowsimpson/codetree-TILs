e6b1644d3dfb3941dd92e219276174998045756b
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-08-31 23:53:18 +0900

    Title: 시험 통과 여부 확인하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250831/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    if(n >= 80){
+        cout << "pass" << endl;
+    }
+    else
+        cout << 80 - n << "more score" << endl;
+    return 0;
+}
