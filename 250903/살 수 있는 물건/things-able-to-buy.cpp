8229e2ed91ba9eefa68c12b250c258e1c0d2848b
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-03 01:16:37 +0900

    Title: 살 수 있는 물건, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    if(n >= 3000){
+        cout << "book" << endl;
+    }else if(n >= 1000){
+        cout << "mask" << endl;
+    }else{
+    cout << "no" << endl;
+    }
+    return 0;
+}
