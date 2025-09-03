0a4f4523f475c38db3eb18a837ca732f44e5edb6
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-03 12:07:33 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/문자 출력하기/print-text.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char n;
+    scanf("%c", &n);
+    
+    for(int i = 0; i < 8; i++){
+        printf("%c", n);
+    }
+
+    return 0;
+}
