daea984f5160d40aa57657d4cd377a49a79e2dae
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-03 12:23:28 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char text;
+    cin >> text;
+
+    for(int i = 0; i < 8; i++){
+        cout << text;
+    }
+    return 0;
+}
