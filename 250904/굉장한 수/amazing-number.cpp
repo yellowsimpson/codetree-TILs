0a2b9e9de02b6cd5efc94130f4b0e61692763305
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-04 01:04:02 +0900

    Title: 굉장한 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250904/굉장한 수/amazing-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    if((n % 2 == 1 && n % 3 == 0) || (n % 2 == 0 && n % 5 == 0)){
+        printf("true");
+    }else{
+        printf("false");
+    }
+    return 0;
+}
