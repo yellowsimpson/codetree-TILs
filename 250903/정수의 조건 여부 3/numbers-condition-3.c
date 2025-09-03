3b6c7da608b691da50a3677aa1f57cf60167e247
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-03 23:14:29 +0900

    Title: 정수의 조건 여부 3, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250903/정수의 조건 여부 3/numbers-condition-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d", &a);
+
+    if(a / 13 == 1 || a / 19 == 1){
+        printf("True");
+    }else
+        printf("False");
+
+
+    return 0;
+}
