526895064fc0fa6c19f062a24ce29e37645829d0
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-03 13:12:12 +0900

    Title: 차례로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/차례로 출력/print-in-order.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n = 4;
+
+    for(int i = 5; i <= 17 ; i++){
+        n += 1;
+        printf("%d ", n);
+    }
+
+    return 0;
+}
