29448ecd6bc2a730b62d233dc3b1043e7b001ec9
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-03 13:20:46 +0900

    Title: 차례로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/차례로 출력/print-in-order.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 4;
+
+    for(int i = 5; i <= 17; i++){
+        n += 1;
+        cout << n << " ";
+    }
+
+    return 0;
+}
