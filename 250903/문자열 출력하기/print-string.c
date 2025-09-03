582967584fb9aeb2f6fa4697ee3a2df8d8428d17
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-03 12:29:22 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/문자열 출력하기/print-string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        printf("LeebrosCode\n");
+    }
+    return 0;
+}
