ab5187ad3e344a19e5eced6e6232639556a5faf4
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-03 15:34:39 +0900

    Title: 정수 입력받아 배수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250903/정수 입력받아 배수 출력/print-multiple-of-input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 1; i <= 5; i++){
+        printf("%d ", n * i);
+    }
+    return 0;
+}
