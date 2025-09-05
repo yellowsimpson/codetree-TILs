056ab746ae80e3a84dd3c245fac20150af4a9ad3
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-05 16:34:50 +0900

    Title: 배열 채우고 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250905/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char arr[10];
+
+    for(int i = 0; i < 10; i++){
+        // scanf(" %c", &arr[i])
+        cin >> arr[i];
+    }
+
+    for(int i = 9; i >= 0; i--){
+        cout <<
+         arr[i];
+    }
+
+    return 0;
+}
