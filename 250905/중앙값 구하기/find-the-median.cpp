57d200bc1d4f0cc647e608618c8b7ab12d70fb11
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-05 14:10:42 +0900

    Title: 중앙값 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250905/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(a > b)
+        if(a > c)
+            if(b > c)
+                cout << b << endl;
+            else
+                cout << c << endl;
+        else
+            cout << a << endl;
+    else
+        if(b > c)
+            if(a > c)
+                cout << a << endl;
+            else
+                cout << c << endl;
+        else
+            cout << b << endl;
+
+    return 0;
+}
