0f4263c31b6bb4f40aeaf230de9539e783311ac3
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-16 22:48:59 +0900

    Title: A부터 B까지 짝수의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250916/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    int cnt = 0;
+
+    scanf("%d %d", &a, &b);
+
+    for(int i = a; i < b; i++){
+        if(i % 2 == 0){
+            cnt += i;
+        }
+    }
+    printf("%d", cnt);
+    return 0;
+}
