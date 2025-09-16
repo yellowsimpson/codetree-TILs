5a6d925313df9eacc3e2a2eea8ddc632aad14c6a
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-16 15:08:29 +0900

    Title: 1부터의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250916/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    int sum = 0;
+
+    cin >> n;
+
+    for(int i = 1; i <= 100; i++) {
+        sum += i;
+        if(sum >= n) {
+            cout << i << endl;
+            break;
+        }
+    }
+    
+    return 0;
+}
+
