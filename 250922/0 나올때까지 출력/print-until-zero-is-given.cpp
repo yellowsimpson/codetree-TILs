b411cc4c8b6227d3f014445b8dc319b0866ada1f
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-22 18:52:19 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    while(1){
+        int num;
+        cin >> num;
+
+        if(num == 0)
+            break;
+        cout << num << endl;
+    }
+    return 0;
+}
