ecf9468ab2e3cb5e4b6421d3c982e0e0bac187f4
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-22 13:30:57 +0900

    Title: 배수의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250922/배수의 개수/number-of-multipliers.cpp
+++ 250922/배수의 개수/number-of-multipliers.cpp
@@ -6,7 +6,7 @@ int main() {
     int num;
     int cnt3 == 0, cnt5 == 0;
 
-    for(int i = 0; i <= 10; i++){
+    for(int i = 1; i <= 10; i++){
         cin >> num;
         if(num % 3 == 0)
             cnt3++;
