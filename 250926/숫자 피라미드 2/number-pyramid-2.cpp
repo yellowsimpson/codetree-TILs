9e40cd2186025146285e40cfdec57efc6ec14ebb
black.w.hite <137969623+yellowsimpson@users.noreply.github.com>
2025-09-26 00:52:56 +0900

    Title: 숫자 피라미드 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250926/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int cnt = 1;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            cout << cnt << " ";
+            cnt++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
