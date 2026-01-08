5f3c941deb5338207bac7dc0de9078b418b7e9d8
black.w.hite <shimsungwhan@naver.com>
2026-01-09 08:19:05 +0900

    Title: 따옴표 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260109/따옴표 출력/print-quote.cpp
+++ 260109/따옴표 출력/print-quote.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    cout << "He says \"It\'s a rally simple sentence\"." "
+    cout << "He says \"It\'s a rally simple sentence\". "
     return 0;
 }
