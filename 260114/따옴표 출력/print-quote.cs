15cc2209541a8f31c3913ead51e66f83193d019d
black.w.hite <shimsungwhan@naver.com>
2026-01-14 22:15:07 +0900

    Title: 따옴표 출력, Time: 52ms, Memory: 4MB, Status: Wrong Answer - Codetree

--- 260114/따옴표 출력/print-quote.cs
+++ 260114/따옴표 출력/print-quote.cs
@@ -5,6 +5,6 @@ public class Codetree
     public static void Main()
     {
         // Please write your code here.
-        Console.Write("He says \"It\'s really simple sentence\".");
+        Console.Write("He says \"It's really simple sentence\".");
     }
 }
