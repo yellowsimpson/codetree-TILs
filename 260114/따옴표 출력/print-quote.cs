484e6e663cac3822c19c3c42aa30092153e9a6a6
black.w.hite <shimsungwhan@naver.com>
2026-01-14 22:14:19 +0900

    Title: 따옴표 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260114/따옴표 출력/print-quote.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        Console.Write("He says \"It\'s really simple sentence\".")
+    }
+}
