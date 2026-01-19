4dd4d7ab7db05e092b826a251d53824d9e2bf947
black.w.hite <shimsungwhan@naver.com>
2026-01-19 14:12:45 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260119/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void 5_starts(){
+    for(int i = 0; i < 10; i++){
+        cin << "*";
+    cout << endl;
+    }
+}
+
+int main() {
+    // Please write your code here.
+    for(int i = 0; i <; i++)
+        5_starts();
+
+    return 0;
+}
