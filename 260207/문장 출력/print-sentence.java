b89e85dfa4f87544daac1e7e477e6c26c639de22
black.w.hite <shimsungwhan@naver.com>
2026-02-07 18:03:10 +0900

    Title: 문장 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260207/문장 출력/print-sentence.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
