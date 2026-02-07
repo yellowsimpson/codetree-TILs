199906e5c2ebaa342e4ae1160dfd7c719eab5550
black.w.hite <shimsungwhan@naver.com>
2026-02-07 18:03:25 +0900

    Title: 문장 출력, Time: 91ms, Memory: 9MB, Status: Passed - Codetree

--- 260207/문장 출력/print-sentence.java
+++ 260207/문장 출력/print-sentence.java
@@ -1,10 +1,3 @@
-public class Main {
-    public static void main(String[] args) {
-        // Please write your code here.
-
-    }
-}
-
 public class Main{
     public static void main(String[] args){
         System.out.print("Hello World!");
